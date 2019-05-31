cff92f23ec9bc7f9456a44f139c12fe22279a70f
0r-et-0 <47462307+0r-et-0@users.noreply.github.com>
2019-05-31 11:00:43 +0200

    Create detectID.js

---
+++ detectID.js
@@ -0,0 +1,17 @@
+let ID;
+let query = window.location.search.substring(1);
+let id = query.split('=')[1];
+switch (id) {
+case '1':
+  ID = 1;
+  break;
+case '2':
+  ID = 2;
+  break;
+case '3':
+  ID = 3;
+  break;
+case '4':
+  ID = 4;
+  break;
+}
