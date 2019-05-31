a8a6f368f3fc1165212ebc907344d930da297e77
0r-et-0 <47462307+0r-et-0@users.noreply.github.com>
2019-05-31 11:33:47 +0200

    Update goToStartPage.js

--- goToStartPage.js
+++ goToStartPage.js
@@ -1,3 +1,5 @@
+//cette fonction envoie les données à votre firebase pas à la database que vous devez créer pour gérer glitch.me
+
 function goToStartPage(){
     send('GOBACK', {
         'goBack' : true
