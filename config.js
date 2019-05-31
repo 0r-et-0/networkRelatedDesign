3dc1ef05679ee1f4a863822e0f76726285f64935
0r-et-0 <47462307+0r-et-0@users.noreply.github.com>
2019-05-31 11:40:37 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+//remplacez avec vos données
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
